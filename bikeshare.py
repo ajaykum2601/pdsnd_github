4c4f280b5fd79c3763da407e99fa23744b74edba
ajay kumar <ajaykum2601@gmail.com>
2022-08-28 08:57:05 +0530

    feat: change 5: changed the print of the data to next 10 rows instead of next 5

--- bikeshare.py
+++ bikeshare.py
@@ -185,15 +185,17 @@ def main():
 	        raw_data =raw_data.lower()
 	        present_count = 0
 	        while (raw_data == 'yes'):
-	        	if (present_count + 5 >= df.shape[0]):
+	        	#print 10 rows of the dataset
+	        	if (present_count + 10 >= df.shape[0]):
 	        		print(df.iloc[list(range(present_count,df.shape[0]-1,1))])
 	        		print("\n End of file reached hence quitting!!!\n")
 	        		break
 	        	else:
-	        		print(df.iloc[list(range(present_count,present_count+5,1))])
-	        		present_count += 5
+	        		print(df.iloc[list(range(present_count,present_count+10,1))])
+	        		present_count += 10
 	        		raw_data = input("Do you want to see raw trip data? Enter <Yes> or <No>")
 	        		raw_data = raw_data.lower()
+
 
 	        
 	        restart = input('\nWould you like to restart? Enter yes or no.\n')
